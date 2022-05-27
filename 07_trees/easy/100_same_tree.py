0da679b1e5a4d7f29b116477e1f5363deda9e365
Dave Lee <isho.lee@gmail.com>
2022-05-27 22:46:52 +0800

    100. same tree

--- 07_trees/easy/100_same_tree.py
+++ 07_trees/easy/100_same_tree.py
@@ -1,6 +1,13 @@
 # 100. Same Tree
 # neetcode: https://youtu.be/vRbbcKXCxOw
 
+# Time: O(p+q)
+
+# Recursive / DFS
+# 1. Check the roots to see if they are the same
+# 2. Check both left sub-trees
+# 3. Check both right sub-trees
+
 # Definition for a binary tree node.
 # class TreeNode(object):
 #     def __init__(self, val=0, left=None, right=None):
@@ -15,3 +22,27 @@ class Solution(object):
         :type q: TreeNode
         :rtype: bool
         """
+
+        # base cases:
+
+        # 1. both are empty trees
+        if not p and not q:
+            return True
+
+        # when passing this point, Not both of them are null
+        # they can be both non-empty, or just one of them is empty
+
+        # 2. Only one of them is empty
+        if not p or not q:
+            return False
+
+        # when passing this point, both of them are non-empty
+
+        # 3. Both nodes are not null, check the values
+        if p.val != q.val:
+            return False
+
+        # when passing this point, both nodes are non-empty and they have the same values
+
+        return (self.isSameTree(p.left, q.left) and
+                self.isSameTree(p.right, q.right))

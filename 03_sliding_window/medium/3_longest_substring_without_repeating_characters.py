f798b01468c6fd96c9949feceee345e1633e2a59
Dave Lee <isho.lee@gmail.com>
2023-11-24 20:07:53 +0800

    3. Longest Substring Without Repeating Characters

---
+++ 03_sliding_window/medium/3_longest_substring_without_repeating_characters.py
@@ -0,0 +1,49 @@
+'''
+3. Longest Substring Without Repeating Characters
+https://leetcode.com/problems/longest-substring-without-repeating-characters/
+
+NeetCode: https://youtu.be/wiGpQwVHdE0?si=6ljPdflpVIEGN6E4
+
+#Medium
+#HashTable #String #Sliding Window
+
+### Related problems:
+
+159. Longest Substring with At Most Two Distinct Characters (M)
+340. Longest Substring with At Most K Distinct Characters (M)
+992. Subarrays with K Different Integers (H)
+
+'''
+
+
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        charSet = set()
+        l = 0
+        res = 0
+
+        for r in range(len(s)):
+            # if we get to a duplicate char, we need to update the window and the set
+            while s[r] in charSet:
+                # remove the left char from our set
+                charSet.remove(s[l])
+                # shrink the window from the left
+                l += 1
+
+            # after removing all duplicate chars:
+            # add the rightmost char to our set
+            charSet.add(s[r])
+            # at this point we are sure our substring/set does not have any duplicates
+            # update our res, which stores the max length so far
+            res = max(res, r-l+1)
+
+        return res
+
+
+solution = Solution()
+
+assert solution.lengthOfLongestSubstring('abcaabcbb') == 3
+assert solution.lengthOfLongestSubstring('bbbbb') == 1
+assert solution.lengthOfLongestSubstring('pwwkew') == 3
+
+print('All tests have passed.')

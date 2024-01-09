920d7c3dc8426f61cbd3d253f7537f264f022475
Dave Lee <isho.lee@gmail.com>
2024-01-09 20:01:25 +0800

    add tree traversal util

--- 07_trees/tree_operations/traverse_tree.py
+++ 07_trees/tree_operations/traverse_tree.py
@@ -1,9 +1,53 @@
 from collections import deque
 
 
-def with_bfs(root):
-    if root is None:
+def preorder_traversal(root):
+    if not root:
         return
+    print(root.val, end=' ')
+    preorder_traversal(root.left)
+    preorder_traversal(root.right)
+
+
+def inorder_traversal(root):
+    if not root:
+        return
+    inorder_traversal(root.left)
+    print(root.val, end=' ')
+    inorder_traversal(root.right)
+
+
+def preorder_traversal_list(root):
+    res = []
+
+    def helper(node):
+        if not node:
+            return
+        res.append(node.val)
+        helper(node.left)
+        helper(node.right)
+
+    helper(root)
+    return res
+
+
+def inorder_traversal_list(root):
+    res = []
+
+    def helper(node):
+        if not node:
+            return
+        helper(node.left)
+        res.append(node.val)
+        helper(node.right)
+
+    helper(root)
+    return res
+
+
+def with_bfs(root):
+    if not root:
+        return None
 
     queue = deque()
     queue.append(root)

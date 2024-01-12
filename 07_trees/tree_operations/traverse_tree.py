39c7bad63419cdf4b72479e0f086b66117d3f40a
Dave Lee <isho.lee@gmail.com>
2024-01-12 18:21:57 +0800

    change output of bfs traversal

--- 07_trees/tree_operations/traverse_tree.py
+++ 07_trees/tree_operations/traverse_tree.py
@@ -1,6 +1,13 @@
 from collections import deque
 
 
+class TreeNode:
+    def __init__(self, val: int = 0, left: 'TreeNode' = None, right: 'TreeNode' = None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
 def preorder_traversal(root):
     if not root:
         return
@@ -51,14 +58,44 @@ def with_bfs(root):
 
     queue = deque()
     queue.append(root)
+    res = []
 
-    while queue:
-        # handle the first node in the queue
-        # pop it from the queue
+    def all_null(q):
+        # 如果 queue 裡面剩下的都是 N  那就沒有必要再繼續處理
+        return all(node.val == 'N' for node in q)
+
+    # 需要繼續處理的例子如 queue: [N, N, 4, 5]  即同一層裡面還有 leaf
+    # 但如果是 [N,N,N,N] 就跳出，例如節點 4 跟 5 會在 queue 裡面 append 四個 N
+    while queue and not all_null(queue):
         current_node = queue.popleft()
-        print(current_node.val, end=' ')
 
+        if (current_node.val == 'N'):
+            # print('None', end=' ')
+            res.append(None)
+        else:
+            # print(current_node.val, end=' ')
+            res.append(current_node.val)
+
+        # left child
         if current_node.left:
-            queue.append(current_node.left)
+            queue.append(current_node.left)  # node or leaf
+        else:
+            queue.append(TreeNode('N'))  # 空節點加入值為 N 的節點
+        # right child
         if current_node.right:
-            queue.append(current_node.right)
+            queue.append(current_node.right)  # node or leaf
+        else:
+            queue.append(TreeNode('N'))  # 空節點加入值為 N 的節點
+
+    return res
+
+    '''
+    #     1
+    #   /   \
+    #  2     3
+    #       / \
+    #      4   5
+
+    # result:
+    # 1 2 3 None None 4 5
+    '''

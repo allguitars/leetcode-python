78324eecbd8a7451266f49ebb3c346e4f4248dae
Dave Lee <isho.lee@gmail.com>
2024-01-13 15:01:05 +0800

    revise code

--- 10_backtracking/39_2024_0113.py
+++ 10_backtracking/39_2024_0113.py
@@ -3,6 +3,9 @@ from typing import List
 
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        # ** NeetCode 的寫法 ======
+        # 每次遞迴呼叫只做一次加法，即 total + candidates[i]
+        # 整體速度會快幾 ms
         res = []
 
         def dfs(i, cur, total):
@@ -19,6 +22,29 @@ class Solution:
 
         dfs(0, [], 0)
         return res
+
+        # ** 簡化寫法，不用傳入那麼多參數 ======
+        # 我自己的寫法，但是每次呼叫 dfs 都要將所有元素加一次  會比上面的方法慢幾 ms
+        # res = []
+        # cur = []
+
+        # def dfs(i):
+        #     # total = sum(cur)
+        #     if sum(cur) == target:
+        #         res.append(cur.copy())
+        #         return
+        #     if i >= len(candidates) or sum(cur) > target:
+        #         return
+
+        #     # 要包含 candidates[i]
+        #     cur.append(candidates[i])
+        #     dfs(i)
+        #     # 一定不含 candidates[i], 只能有 i+1 之後的數字
+        #     cur.pop()
+        #     dfs(i+1)
+
+        # dfs(0)
+        # return res
 
 
 s = Solution()

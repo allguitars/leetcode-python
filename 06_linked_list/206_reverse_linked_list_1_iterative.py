a822c5cb7a757b110e7d17a72fbc3bd37063a77e
Dave Lee <isho.lee@gmail.com>
2024-01-13 13:47:30 +0800

    change folder structure

--- 06_linked_list/206_reverse_linked_list_1_iterative.py
+++ 06_linked_list/206_reverse_linked_list_1_iterative.py
@@ -1,15 +1,22 @@
-# 206. Reverse Linked List
-# neetcode: https://youtu.be/G0_I-ZF0S38
+'''
+206. Reverse Linked List
+https://leetcode.com/problems/reverse-linked-list/description/
 
-# Time: O(n)
-# Space: O(1)
+Neetcode: https://youtu.be/G0_I-ZF0S38
 
+Time: O(n)
+Space: O(1)
 
-# Definition for singly-linked list.
-# class ListNode(object):
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+Definition for singly-linked list.
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+#Easy
+#LinkedList #Recursion
+'''
+
 
 class Solution(object):
     def reverseList(self, head):
---
+++ 06_linked_list/206_reverse_linked_list_2_recursive.py
@@ -0,0 +1,28 @@
+'''
+206. Reverse Linked List
+https://leetcode.com/problems/reverse-linked-list/description/
+
+Neetcode: https://youtu.be/G0_I-ZF0S38
+
+Time: O(n)
+Space: O(1)
+
+Definition for singly-linked list.
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+#Easy
+#LinkedList #Recursion
+'''
+
+
+class Solution(object):
+    def reverseList(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+
+        # TODO

87bf279ff90a6454c311035da981cdf4ce7e7d3f
Dave Lee <isho.lee@gmail.com>
2024-01-13 13:25:34 +0800

    39. Combination Sum

--- 10_backtracking/39_combination_sum.py
+++ 10_backtracking/39_combination_sum.py
@@ -19,28 +19,27 @@ from typing import List
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         res = []
-        current = []
 
-        def dfs(i):
-            if sum(current) == target:
-                res.append(current.copy())
+        def dfs(i, cur, total):
+            if sum(cur) == target:
+                res.append(cur.copy())
                 return
-            if i >= len(candidates) or sum(current) > target:
+            if i >= len(candidates) or sum(cur) > target:
                 return
 
             # include item at index i to the sub-array
-            current.append(candidates[i])
+            cur.append(candidates[i])
 
             # 1. decision to keep adding current item -- do not change i
-            dfs(i)
+            dfs(i, cur, total + candidates[i])
 
             # restore the sub-arr after recursive call for making other decision
-            current.pop()
+            cur.pop()
 
             # 2. decision NOT to go with current item anymore -- increment i
-            dfs(i+1)
+            dfs(i+1, cur, total)
 
-        dfs(0)
+        dfs(0, [], 0)
         return res
 
 
@@ -49,6 +48,5 @@ s = Solution()
 # candidates, target
 print(s.combinationSum([2, 3, 5], 8))
 
-# 這個方法無法排除相同組合但順序不同的  例如 [3,5] and [5,3]
 # Output:
 # [[2, 2, 2, 2], [2, 3, 3], [3, 5]]

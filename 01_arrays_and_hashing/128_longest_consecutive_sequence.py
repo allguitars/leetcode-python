8f97f444418b8fadbf632fabdd21bd3d7a0d1423
Dave Lee <isho.lee@gmail.com>
2024-01-24 19:07:10 +0800

    change project structure

---
+++ 01_arrays_and_hashing/128_longest_consecutive_sequence.py
@@ -0,0 +1,16 @@
+'''
+128. Longest Consecutive Sequence
+https://leetcode.com/problems/longest-consecutive-sequence/
+
+NeetCode: https://youtu.be/P6RZZMu_maU
+
+#Medium
+#Array #HashTable #UnionFind
+'''
+
+from typing import List
+
+
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        pass
